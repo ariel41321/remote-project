453c5f5679025afb833374aef0dcd08adcceaf9e
Chihhan <ariel41321@gmail.com>
2020-09-27 16:39:42 +0800

    assignment completed-version1

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 const app = express();
+
+app.use(express.urlencoded( {extended: false} ));
+app.use(cookieParser());
 
 app.use(express.static('public'));
 
@@ -27,6 +31,51 @@ app.get('/getData', (req, res) => {
 	    }
 	res.send(`${result}`);
 });
+
+//Assignment4
+app.get('/myName', (req, res) => {
+	const username = req.cookies.name;
+	if (username) {
+		res.render('index', { name:username });
+	} else {
+		res.redirect('/trackName');
+	}
+});
+
+// app.get('/trackName', (req, res) => {
+// 	res.render('trackname', { name: req.cookies.username });
+// });
+
+app.get('/trackName', (req, res) => {
+	const username = req.cookies.name;
+	if (username) {
+		res.redirect('/myName');
+	} else {
+		res.render('trackname');
+	}
+});
+
+// app.all('/trackName', (req, res) => {
+//     console.log(req.body)
+//     res.render('trackname', { name: req.body.username });
+// });
+
+// app.post('/trackName', (req, res) => {
+// 	res.cookie('username', req.body.username);
+// 	console.log(req.body);
+// 	res.render('trackname', { name: req.body.username });
+// });
+
+app.post('/trackName', (req, res) => {
+	res.cookie('name', req.body.name);
+	res.redirect('/myName');
+});
+
+app.post('/reset', (req, res) => {
+	res.clearCookie('name');
+	res.redirect('/trackName');
+});
+
 
 app.listen(3000, () => {
 	console.log('the app is running on localhost:3000');
